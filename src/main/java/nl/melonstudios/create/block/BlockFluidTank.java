fbda36a99fedd0911e1237b52431a1e4d8debe79
Sekai0NI0itamio <72232@mail.ssis-suzhou.net>
2026-09-15 21:10:27 +0800

    Fix missing Block import in tank

--- src/main/java/nl/melonstudios/create/block/BlockFluidTank.java
+++ src/main/java/nl/melonstudios/create/block/BlockFluidTank.java
@@ -1,5 +1,6 @@
 package nl.melonstudios.create.block;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
